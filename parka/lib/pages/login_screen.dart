3002d010740a03830bd0709252a1a3c9f49bb050
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-08-03 13:01:39 -0400

    started working on facebook button

--- parka/lib/pages/login_screen.dart
+++ parka/lib/pages/login_screen.dart
@@ -14,18 +14,52 @@ class LoginScreen extends StatelessWidget {
           ),
           Expanded(
               flex: 2,
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    "Bienvenido a ParkA",
-                    style: TextStyle(
-                        color: Color(0xFF0B768C),
-                        fontSize: 48.0,
-                        fontFamily: "Righteous",
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text("Simplificando la busqueda y alquiler de parqueos")
-                ],
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Bienvenido a ParkA",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Color(0xFF0B768C),
+                          fontSize: 48.0,
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Simplificando la busqueda y alquiler de parqueos",
+                      style: TextStyle(
+                          color: Color(0xFF0B768C),
+                          fontFamily: "Montserrat",
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      height: 32.0,
+                      width: 300.0,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF3B5998),
+                          borderRadius:
+                              BorderRadius.all(Radius.circular(50.0))),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                        child: Row(
+                          children: <Widget>[
+                            Image.asset("images/facebook_logo.png"),
+                            Text(
+                              "Iniciar sesion con Facebook",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontFamily: "Montserrat",
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.bold),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ))
         ],
       ),
