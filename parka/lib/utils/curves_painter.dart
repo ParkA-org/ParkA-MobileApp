af912c3addeb8ca22f1775d6f1982e841bc0325a
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-08-03 23:10:30 -0400

    changed wave second point height

--- parka/lib/utils/curves_painter.dart
+++ parka/lib/utils/curves_painter.dart
@@ -26,7 +26,7 @@ class BottomWaveClipper extends CustomClipper<Path> {
         firstEndPoint.dx, firstEndPoint.dy);
 
     var secondControlPoint =
-        Offset(size.width - (size.width / 3.25), size.height - 65);
+        Offset(size.width - (size.width / 3.25), size.height - 60);
     var secondEndPoint = Offset(size.width, size.height - 40);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
@@ -39,5 +39,5 @@ class BottomWaveClipper extends CustomClipper<Path> {
   }
 
   @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
