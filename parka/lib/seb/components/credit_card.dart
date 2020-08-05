f6a7a67dec0ad811160e1adcffff8fd2a4203d9c
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-08-04 23:50:09 -0400

    completed basic functionalities of payment page

---
+++ parka/lib/seb/components/credit_card.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class CreditCardWidget extends StatelessWidget {
+  final String fullName;
+  final String creditCardNumber1;
+  final String creditCardNumber2;
+  final String creditCardNumber3;
+  final String creditCardNumber4;
+  final String creditCardMonth;
+  final String creditCardYear;
+
+  const CreditCardWidget(
+      {Key key,
+      this.fullName,
+      this.creditCardNumber1,
+      this.creditCardNumber2,
+      this.creditCardNumber3,
+      this.creditCardNumber4,
+      this.creditCardMonth,
+      this.creditCardYear})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Color(0xFF265c88),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+      child: Container(
+        height: 190.0,
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Icon(
+                  FontAwesomeIcons.ccVisa,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            Text(
+              "${this.creditCardNumber1} ${this.creditCardNumber2} ${this.creditCardNumber3} ${this.creditCardNumber4}",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  color: Colors.white,
+                  wordSpacing: 10.0,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Text(
+                    this.fullName,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "Valido \n hasta",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Expanded(
+                  child: Text(
+                    "${this.creditCardMonth}/${this.creditCardYear}",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- parka/lib/seb/pages/payment_info.dart
+++ parka/lib/seb/pages/payment_info.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "../utils/curves_painter.dart";
+import "../components/credit_card.dart";
 
 class PaymentInfoScreen extends StatefulWidget {
   @override
@@ -7,34 +8,237 @@ class PaymentInfoScreen extends StatefulWidget {
 }
 
 class _PaymentInfoScreenState extends State<PaymentInfoScreen> {
+  String fullName = "Nombre del titular";
+  String creditCardNumber1 = "----";
+  String creditCardNumber2 = "----";
+  String creditCardNumber3 = "----";
+  String creditCardNumber4 = "----";
+  String creditCardMonth = "--";
+  String creditCardYear = "--";
+  String creditCardCvv;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       backgroundColor: Color(0xFF0B768C),
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
-                flex: 4,
+                flex: 7,
                 child: WavyHeaderImage(
                   child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20.0),
                     color: Colors.white,
-                    child: Column(
-                      children: <Widget>[
-                        TransparentButtonWithIcon(
-                          label: "Atras",
-                          color: Color(0xFF0B768C),
-                        ),
-                        Text(
-                          "Metodo de pago",
-                          style: TextStyle(
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 50.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          TransparentButtonWithIcon(
+                            label: "Atras",
+                            color: Color(0xFF0B768C),
+                          ),
+                          Text(
+                            "Metodo de pago",
+                            style: TextStyle(
+                                color: Color(0xFF0B768C),
+                                fontSize: 32.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          CreditCardWidget(
+                            fullName: this.fullName,
+                            creditCardNumber1: this.creditCardNumber1,
+                            creditCardNumber2: this.creditCardNumber2,
+                            creditCardNumber3: this.creditCardNumber3,
+                            creditCardNumber4: this.creditCardNumber4,
+                            creditCardMonth: this.creditCardMonth,
+                            creditCardYear: this.creditCardYear,
+                          ),
+                          Text(
+                            "Numero Tarjeta",
+                            style: TextStyle(
                               color: Color(0xFF0B768C),
-                              fontSize: 32.0,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        CreditCardWidget()
-                      ],
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: kInputStyle,
+                                    keyboardType: TextInputType.number,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        this.creditCardNumber1 = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: kInputStyle,
+                                    keyboardType: TextInputType.number,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        this.creditCardNumber2 = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: kInputStyle,
+                                    keyboardType: TextInputType.number,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        this.creditCardNumber3 = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: TextField(
+                                    decoration: kInputStyle,
+                                    keyboardType: TextInputType.number,
+                                    onChanged: (value) {
+                                      setState(() {
+                                        this.creditCardNumber4 = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Nombre del titular",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              color: Color(0xFF0B768C),
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          TextField(
+                            decoration: kInputStyle,
+                            onChanged: (value) {
+                              setState(() {
+                                this.fullName = value;
+                              });
+                            },
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: <Widget>[
+                                    Text(
+                                      "Valido Hasta",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        color: Color(0xFF0B768C),
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Row(
+                                      children: <Widget>[
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              decoration: kInputStyle,
+                                              keyboardType:
+                                                  TextInputType.number,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  this.creditCardMonth = value;
+                                                });
+                                              },
+                                            ),
+                                          ),
+                                        ),
+                                        Text(
+                                          "/",
+                                          style: TextStyle(
+                                            color: Color(0xFF0B768C),
+                                            fontSize: 25.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        Expanded(
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: TextField(
+                                              decoration: kInputStyle,
+                                              keyboardType:
+                                                  TextInputType.number,
+                                              onChanged: (value) {
+                                                setState(() {
+                                                  this.creditCardYear = value;
+                                                });
+                                              },
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  crossAxisAlignment:
+                                      CrossAxisAlignment.stretch,
+                                  children: <Widget>[
+                                    Text(
+                                      "CVV",
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                        color: Color(0xFF0B768C),
+                                        fontSize: 18.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: TextField(
+                                        decoration: kInputStyle,
+                                        keyboardType: TextInputType.number,
+                                        onChanged: (value) {
+                                          setState(() {
+                                            this.creditCardCvv = value;
+                                          });
+                                          print(this.creditCardCvv);
+                                        },
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 )),
@@ -53,54 +257,6 @@ class _PaymentInfoScreenState extends State<PaymentInfoScreen> {
                   ),
                 ],
               ),
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CreditCardWidget extends StatelessWidget {
-  const CreditCardWidget({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      color: Color(0xFF265c88),
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-      child: Container(
-        height: 190.0,
-        width: 350.0,
-        padding: EdgeInsets.symmetric(horizontal: 15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Icon(Icons.credit_card),
-            Text(
-              "5333 6666 7777 9999",
-              textAlign: TextAlign.left,
-              style: TextStyle(color: Colors.white),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  "Silvio Arzeno",
-                  style: TextStyle(color: Colors.white),
-                ),
-                Text(
-                  "Valido \n hasta",
-                  style: TextStyle(color: Colors.white),
-                ),
-                Text(
-                  "05/24",
-                  style: TextStyle(color: Colors.white),
-                )
-              ],
             )
           ],
         ),
@@ -166,3 +322,22 @@ class TransparentButtonWithRightIcon extends StatelessWidget {
     );
   }
 }
+
+const kInputStyle = InputDecoration(
+  hintText: '',
+  hintStyle: TextStyle(color: Colors.grey),
+  filled: true,
+  fillColor: Color(0xFFD7D2D2),
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.lightBlueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+  ),
+);
