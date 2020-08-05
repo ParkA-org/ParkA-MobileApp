1dc3c29462989b0218d3e23ab8322b587cd29a9f
Silvio Daniel Arzeno <42903129+SilvioArzeno@users.noreply.github.com>
2020-08-05 19:57:32 -0400

    Update main.dart

--- parka/lib/main.dart
+++ parka/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import "./routes/routes.dart";
 
 void main() {
   runApp(MyApp());
@@ -21,14 +20,13 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.lightBlue,
+        primarySwatch: Colors.lightBlue[800],
         // This makes the visual density adapt to the platform that you run
         // the app on. For desktop platforms, the controls will be smaller and
         // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      // home: MyHomePage(title: 'ParkA'),
-      routes: routes,
+      home: MyHomePage(title: 'ParkA'),
     );
   }
 }
