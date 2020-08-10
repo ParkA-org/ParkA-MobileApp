b1a61a6ca442c2b97add1780b0b6438e61737751
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-08-10 18:27:02 -0400

    PMB-8 Finished last Draft for Proifle Pic Page

--- parka/lib/components/Utils/curves_painter.dart
+++ parka/lib/components/Utils/curves_painter.dart
@@ -21,6 +21,9 @@ class WavyHeaderImage extends StatelessWidget {
       case 'Registration':
         selectedClipper = BottomWaveClipper();
         break;
+      case 'IDPage':
+        selectedClipper = IDPageClipper();
+        break;
 
       default:
         selectedClipper = BottomWaveClipper();
@@ -32,6 +35,60 @@ class WavyHeaderImage extends StatelessWidget {
       clipper: selectedClipper,
     );
   }
+}
+
+class IDPageClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    final double _xScaling = size.width / 375;
+    final double _yScaling = size.height / 200;
+    path.lineTo(0.0000228882 * _xScaling, 33.7588 * _yScaling);
+    path.cubicTo(
+      0.00000727075 * _xScaling,
+      -61.5327 * _yScaling,
+      375 * _xScaling,
+      80.0496 * _yScaling,
+      375 * _xScaling,
+      33.7588 * _yScaling,
+    );
+    path.cubicTo(
+      375 * _xScaling,
+      -12.532 * _yScaling,
+      375 * _xScaling,
+      210 * _yScaling,
+      375 * _xScaling,
+      210 * _yScaling,
+    );
+    path.cubicTo(
+      375 * _xScaling,
+      210 * _yScaling,
+      0 * _xScaling,
+      210 * _yScaling,
+      0 * _xScaling,
+      210 * _yScaling,
+    );
+    path.cubicTo(
+      0 * _xScaling,
+      210 * _yScaling,
+      0.0000385056 * _xScaling,
+      129.05 * _yScaling,
+      0.0000228882 * _xScaling,
+      33.7588 * _yScaling,
+    );
+    path.cubicTo(
+      0.0000228882 * _xScaling,
+      33.7588 * _yScaling,
+      0.0000228882 * _xScaling,
+      33.7588 * _yScaling,
+      0.0000228882 * _xScaling,
+      33.7588 * _yScaling,
+    );
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => true;
 }
 
 class BottomWaveClipper extends CustomClipper<Path> {
