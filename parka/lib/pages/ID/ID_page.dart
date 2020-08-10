590dd18f2475766bdf4b5f997ba93a3cf0cf2b54
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-08-10 18:30:34 -0400

    PMB-9 Initial Page Commit

---
+++ parka/lib/pages/ID/ID_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class IDPage extends StatefulWidget {
+  IDPage({Key key}) : super(key: key);
+
+  @override
+  _IDPageState createState() => _IDPageState();
+}
+
+class _IDPageState extends State<IDPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Hello'),
+    );
+  }
+}
