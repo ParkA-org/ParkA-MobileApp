f4355647ea926c8c25872fc3192af9052c4b027d
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-08-12 12:44:25 -0400

    PMB-9 Changes to state

--- parka/lib/pages/ID/ID_page.dart
+++ parka/lib/pages/ID/ID_page.dart
@@ -11,7 +11,7 @@ class _IDPageState extends State<IDPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Hello'),
+      child: Scaffold(body: Text('Hello')),
     );
   }
 }
--- parka/lib/routes/routes.dart
+++ parka/lib/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:ParkA/pages/ID/ID_page.dart';
 import 'package:ParkA/pages/Login/email_login.dart';
 import 'package:ParkA/pages/Login/login_screen.dart';
 import 'package:flutter/material.dart';
@@ -5,4 +6,5 @@ import 'package:flutter/material.dart';
 Map<String, WidgetBuilder> routes = {
   "/": (BuildContext context) => LoginScreen(),
   "/EmailLogin": (BuildContext context) => LoginPage(),
+  "/IDPage": (BuildContext context) => IDPage(),
 };
