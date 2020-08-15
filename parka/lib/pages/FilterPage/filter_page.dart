57468475c6f505e3f9e1f762209b237dfd9b14a2
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-08-15 19:38:32 -0400

    created base file for filter page

---
+++ parka/lib/pages/FilterPage/filter_page.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class FilterPage extends StatefulWidget {
+  static String routeName = "/parkSearchFilterPage";
+
+  @override
+  _FilterPageState createState() => _FilterPageState();
+}
+
+class _FilterPageState extends State<FilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
