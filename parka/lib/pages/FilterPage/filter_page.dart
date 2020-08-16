408420db88b41e5bc37178d300e1d581351910f7
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-08-15 20:38:56 -0400

    created base layout for filter page

--- parka/lib/pages/FilterPage/filter_page.dart
+++ parka/lib/pages/FilterPage/filter_page.dart
@@ -8,8 +8,101 @@ class FilterPage extends StatefulWidget {
 }
 
 class _FilterPageState extends State<FilterPage> {
+  double rentPriceFilter = 200.0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(child: Text("Puntuacion Minima")),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Icon(Icons.star),
+                          Icon(Icons.star),
+                          Icon(Icons.star),
+                          Icon(Icons.star),
+                          Icon(Icons.star)
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(child: Text("Precio")),
+                    Expanded(
+                      child: Slider(
+                        value: this.rentPriceFilter,
+                        min: 50.0,
+                        max: 1000.0,
+                        onChanged: (value) {
+                          setState(
+                            () {
+                              this.rentPriceFilter = value;
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(child: Text("Tipo dereserva")),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Expanded(child: Text("Horas")),
+                          Expanded(child: Text("Dias")),
+                          Expanded(child: Text("Semanas")),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Expanded(child: Text("Disponibilidad")),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Text("Fecha"),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Text("Horas"),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: ExpansionTile(
+                  title: Text("Caracteristicas"),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
