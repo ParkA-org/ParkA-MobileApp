35ef662dac4c3771efae6f2bd07934f7e5f6659d
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-08-24 17:57:53 -0400

    created features section, added icons, animation and styled

--- parka/lib/pages/FilterPage/filter_page.dart
+++ parka/lib/pages/FilterPage/filter_page.dart
@@ -1,4 +1,5 @@
 import 'package:ParkA/pages/FilterPage/components/buttons_reservation_type_widget.dart';
+import 'package:ParkA/pages/FilterPage/components/featureFilterWidget/feature_filter_widget.dart';
 import "package:flutter/material.dart";
 
 import 'components/slider_price_widget.dart';
@@ -15,7 +16,24 @@ class _FilterPageState extends State<FilterPage> {
   double rentPriceFilter = 200.0;
   int parkingVoteFilter = 4;
   List<String> parkingTypeReservation = new List();
-  List<bool> isSelected = [false, false, false];
+  List<String> parkingFeatureList = new List();
+  List<bool> isSelectedParkingType = [false, false, false];
+  List<String> iconNames = [
+    "camFeature.svg",
+    "accessControlFeature.svg",
+    "guardFeature.svg",
+    "carWashFeature.svg",
+    "carChargeFeature.svg",
+    "valetParkingFeature.svg",
+  ];
+  List<bool> isSelectedParkingFeature = [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false
+  ];
 
   void changeParkingVoteFilter(int vote) {
     setState(() {
@@ -33,8 +51,8 @@ class _FilterPageState extends State<FilterPage> {
 
   void changeParkingTypeFilter(String type, int index) {
     setState(() {
-      this.isSelected[index] = !this.isSelected[index];
-      if (!this.isSelected[index]) {
+      this.isSelectedParkingType[index] = !this.isSelectedParkingType[index];
+      if (!this.isSelectedParkingType[index]) {
         this.parkingTypeReservation.remove(type);
         return;
       }
@@ -45,65 +63,102 @@ class _FilterPageState extends State<FilterPage> {
     print(this.parkingTypeReservation);
   }
 
+  void changeParkingFeatureFilter(String type, int index) {
+    setState(() {
+      this.isSelectedParkingFeature[index] =
+          !this.isSelectedParkingFeature[index];
+      if (!this.isSelectedParkingFeature[index]) {
+        this.parkingFeatureList.remove(type);
+        return;
+      }
+
+      this.parkingFeatureList.add(type);
+    });
+
+    print(this.parkingFeatureList);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
-            children: [
-              Expanded(
-                child: StarRatingFilter(
-                  rating: this.parkingVoteFilter,
-                  rateHandler: this.changeParkingVoteFilter,
+        child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
                 ),
-              ),
-              Expanded(
-                child: PriceSliderWidget(
-                  rentPriceFilter: rentPriceFilter,
-                  sliderChangeHandler: changeParkingPriceFilter,
-                ),
-              ),
-              Expanded(
-                child: ReserveTypeSelectorWidget(
-                  types: [
-                    "Horas",
-                    "Dias",
-                    "Semanas",
-                  ],
-                  selectedTypes: this.isSelected,
-                  onPressedHandler: this.changeParkingTypeFilter,
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Expanded(child: Text("Disponibilidad")),
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Text("Fecha"),
-                        ],
-                      ),
+                child: IntrinsicHeight(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                    child: Column(
+                      children: <Widget>[
+                        Expanded(
+                          child: StarRatingFilter(
+                            rating: this.parkingVoteFilter,
+                            rateHandler: this.changeParkingVoteFilter,
+                          ),
+                        ),
+                        Expanded(
+                          child: PriceSliderWidget(
+                            rentPriceFilter: rentPriceFilter,
+                            sliderChangeHandler: changeParkingPriceFilter,
+                          ),
+                        ),
+                        Expanded(
+                          child: ReserveTypeSelectorWidget(
+                            types: [
+                              "Horas",
+                              "Dias",
+                              "Semanas",
+                            ],
+                            selectedTypes: this.isSelectedParkingType,
+                            onPressedHandler: this.changeParkingTypeFilter,
+                          ),
+                        ),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Expanded(child: Text("Disponibilidad")),
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Text("Fecha"),
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Text("Horas"),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        FeatureSelectorFilter(
+                          renderAvaliableWidth: viewportConstraints.maxWidth,
+                          onTapHandler: this.changeParkingFeatureFilter,
+                          selectedFeatures: this.isSelectedParkingFeature,
+                          featureTypes: [
+                            "Camara de seguridad",
+                            "Control de acceso",
+                            "Vigilancia",
+                            "Car Wash",
+                            "Puerto de Carga",
+                            "Valet Parking"
+                          ],
+                          iconNames: this.iconNames,
+                        )
+                      ],
                     ),
-                    Expanded(
-                      child: Row(
-                        children: [
-                          Text("Horas"),
-                        ],
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-              Expanded(
-                child: ExpansionTile(
-                  title: Text("Caracteristicas"),
-                ),
-              )
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
