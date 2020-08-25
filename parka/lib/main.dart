08f47cf4debca0b065d306ef0e8f6c569830671b
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-08-24 22:44:17 -0400

    PMB-16 Change to ID Page

--- parka/lib/main.dart
+++ parka/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:ParkA/pages/ForgotPassword/forgot_password_screen.dart';
+import 'package:ParkA/pages/ID/ID_page.dart';
 import 'package:ParkA/pages/MapPage/map_page.dart';
 
 import 'package:flutter/material.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       routes: routes,
-      initialRoute: MapPage.routeName,
+      initialRoute: IDPage.routeName,
     );
   }
 }
