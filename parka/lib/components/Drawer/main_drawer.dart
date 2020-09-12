5d0b4e7a35b8befd51275b0483066fce4ae82d1a
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-09-11 20:24:13 -0400

    PMB-17 lays out all the components on the main drawer

--- parka/lib/components/Drawer/main_drawer.dart
+++ parka/lib/components/Drawer/main_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:ParkA/components/Utils/styles/parka_colors.dart';
+import 'package:ParkA/components/Utils/styles/text.dart';
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
@@ -6,27 +7,166 @@ class MainDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
     return Drawer(
       child: SafeArea(
-        child: Container(
-          color: ParkaColors.parkaGreen,
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Icon(
-                      Icons.account_circle_outlined,
-                      color: Colors.white,
-                      size: 100,
-                    )
-                  ],
-                ),
-              )
-            ],
+        child: Stack(alignment: Alignment.bottomCenter, children: [
+          Column(children: [
+            Container(
+              height: screenSize.height * 0.42,
+              color: ParkaColors.parkaGreen,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Icon(
+                              Icons.account_circle_outlined,
+                              color: Colors.white,
+                              size: 50,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 6.5),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(
+                                    width: screenSize.width * 0.46,
+                                    child: Text(
+                                      "Sebastiano Faiella",
+                                      style: kParkaInputDefaultSyle.copyWith(
+                                          fontSize: 16),
+                                      softWrap: false,
+                                      overflow: TextOverflow.fade,
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: [
+                                      Text(
+                                        "4.20",
+                                        style: kParkaInputDefaultSyle.copyWith(
+                                          fontWeight: FontWeight.normal,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        width: 5,
+                                      ),
+                                      Padding(
+                                        padding:
+                                            const EdgeInsets.only(bottom: 2),
+                                        child: Icon(Icons.star_border,
+                                            color: Colors.white, size: 20),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Divider(
+                        thickness: 1,
+                        color: Colors.grey,
+                        indent: 15,
+                        endIndent: 15,
+                      ),
+                    ]),
+              ),
+            ),
+          ]),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 50),
+            child: Container(
+              height: screenSize.height * 0.75,
+              width: screenSize.width * 0.6,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Mis Reservas",
+                    style: kParkaInputDefaultSyle,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Mis Parqueos",
+                    style: kParkaInputDefaultSyle,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Mis Vehiculos",
+                    style: kParkaInputDefaultSyle,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Mis Reservas",
+                    style: kParkaInputDefaultSyle,
+                    textAlign: TextAlign.start,
+                  ),
+                  SizedBox(
+                    height: screenSize.height * 0.001,
+                  ),
+                  Text(
+                    "Metodos de Pago",
+                    style: kParkaInputDefaultStyleBlue,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Chats",
+                    style: kParkaInputDefaultStyleBlue,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Buscar Parqueo",
+                    style: kParkaInputDefaultStyleBlue,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Registra tu Parqueo",
+                    style: kParkaInputDefaultStyleBlue,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Ayuda",
+                    style: kParkaInputDefaultStyleBlue,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Configuracion",
+                    style: kParkaInputDefaultStyleBlue,
+                    textAlign: TextAlign.start,
+                  ),
+                  Text(
+                    "Salir",
+                    style: kParkaInputDefaultStyleBlue,
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+          Container(
+            width: screenSize.width * 0.6,
+            margin: EdgeInsets.only(bottom: 10),
+            child: Text(
+              "Terminos y condiciones legales",
+              textAlign: TextAlign.left,
+              style: kParkaInputDefaultStyleBlue.copyWith(
+                  fontWeight: FontWeight.normal, fontSize: 12),
+            ),
+          )
+        ]),
       ),
     );
   }
--- parka/lib/main.dart
+++ parka/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:ParkA/components/Utils/graphql/graphql_client.dart';
 import 'package:ParkA/pages/Login/login_screen.dart';
+import 'package:ParkA/pages/MapPage/maps_page.dart';
 
 import 'package:flutter/material.dart';
 import 'routes/routes.dart';
@@ -21,7 +22,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       routes: routes,
-      initialRoute: LoginScreen.routeName,
+      initialRoute: MapPage.routeName,
     );
   }
 }
