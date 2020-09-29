e653eda71c0f753ac3e2f6524783b2f0d290f4f2
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-09-29 13:57:28 -0400

    created user use cases class

---
+++ parka/lib/use-cases/user/user_use_cases.dart
@@ -0,0 +1,30 @@
+import 'package:ParkA/components/Utils/graphql/graphql_client.dart';
+import 'package:graphql/client.dart';
+
+class UserUseCases {
+  Future userLogin(String email, String password) async {
+    final loginInput = {
+      "input": {"email": "seb97@gmail.com", "password": "1234567890"}
+    };
+
+    final String loginMutation = r'''
+    mutation($input:LoginUserInput!){
+      login(loginUserInput:$input){
+        JWT,
+        user{
+          name
+          email
+          }
+        }
+      }
+    ''';
+
+    MutationOptions loginMutationOptions = MutationOptions(
+      documentNode: gql(loginMutation),
+      variables: loginInput,
+    );
+
+    final QueryResult loginResult =
+        await ParkaGraphqlClient.graphQlClient.mutate(loginMutationOptions);
+  }
+}
