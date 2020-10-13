71570ba78f408b2cc23310dab5421fafd96a4966
David <davidbujosa@gmail.com>
2020-10-13 07:34:19 -0400

    Add new page

---
+++ parka/lib/pages/VehiclePage/vehicle_page.dart
@@ -0,0 +1,85 @@
+import 'package:ParkA/components/Utils/styles/parka_colors.dart';
+import 'package:ParkA/pages/PaymentInfo/payment_info.dart';
+import "package:flutter/material.dart";
+import 'package:get/get.dart';
+import '../../components/Utils/parkaIcons.dart';
+
+class VehicleListScreen extends StatefulWidget {
+  static const String routeName = "/user-payment-methods";
+
+  @override
+  __VehicleListScreenState createState() =>
+      __VehicleListScreenState();
+}
+
+class __VehicleListScreenState extends State<VehicleListScreen> {
+  // final graphqlClient = Get.find<GraphqlClientController>();
+  bool vehiclesLoaded;
+  List userVehicles;
+
+  Future getAllUserVehicle() async {
+    this.userVehicles = await PaymentUseCases.getAllUserPaymentMethods();
+    setState(() {
+      this.vehiclesLoaded = true;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.paymentsLoaded = false;
+    this.getAllUserPayment();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: ParkaColors.parkaGreen,
+        child: Icon(Icons.add),
+        onPressed: () {
+          Get.toNamed(PaymentInfoScreen.routeName);
+        },
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: ParkaColors.parkaGreen,
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(15.0),
+                      bottomRight: Radius.circular(15.0),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        offset: Offset(3.0, 7.0),
+                        color: Colors.black38,
+                        blurRadius: 5.0,
+                      ),
+                    ]),
+                alignment: Alignment.centerLeft,
+                child: Icon(
+                  ParkaIcons.parkaCar,
+                  color: Colors.white,
+                  size: 130.0,
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32.0),
+                child: this.paymentsLoaded ? ListView() : Container(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
