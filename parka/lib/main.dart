97b8d38c6cd15b82dd75824f15f1f9b8e5a97e0f
David <davidbujosa@gmail.com>
2020-10-13 13:50:31 -0400

    v:

--- parka/lib/main.dart
+++ parka/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:ParkA/components/Utils/graphql/graphql_client.dart';
 import 'package:ParkA/pages/MapPage/maps_page.dart';
-import 'package:ParkA/pages/VehiclePage/vehicle_page.dart';
 
 import 'package:flutter/material.dart';
 import 'routes/routes.dart';
@@ -22,7 +21,7 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       routes: routes,
-      initialRoute: VehiclePage.routeName,
+      initialRoute: MapPage.routeName,
     );
   }
 }
