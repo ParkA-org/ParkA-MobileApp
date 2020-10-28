7693fa23aed85768f60c8c84f474f2548d8ef26a
David <davidbujosa@gmail.com>
2020-10-28 19:39:28 -0400

    Add new font family

--- parka/lib/pages/user-payments/components/card_preview_widget.dart
+++ parka/lib/pages/user-payments/components/card_preview_widget.dart
@@ -20,27 +20,68 @@ class CardListTile extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Text(
-            this.payment.cardHolder,
-            style: kParkaTextBaseStyleWhite.copyWith(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 36.0,
+          Padding(
+            padding: const EdgeInsets.only(left: 14.0),
+            child: Text(
+              this.payment.cardHolder,
+              style: kParkaTextBaseStyleWhite.copyWith(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 36.0,
+              ),
             ),
           ),
           Row(
             children: [
-              Text("text"),
               Column(
                 children: [
+                  this.payment.card == "Visa"
+                      ? Image.network(
+                          'https://parka-api-bucket-aws.s3.amazonaws.com/image_68_1915d643da.png',
+                          width: 100,
+                        )
+                      : Image.network(
+                          'https://parka-api-bucket-aws.s3.amazonaws.com/image_59_159f189071.png',
+                          width: 100,
+                        )
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
                   Text(
-                    "•••• $visible",
+                    " • • • •  $visible",
                     style: kParkaTextBaseStyleWhite.copyWith(
                       color: Colors.black,
-                      fontWeight: FontWeight.w600,
+                      fontFamily: "Montserrat",
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
-                  Text("test"),
+                  Row(
+                    children: [
+                      Text(
+                        "Estado: ",
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          color: Color(0xff888383),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      this.payment.activated == true
+                          ? Text("Aprobada",
+                              style: TextStyle(
+                                  color: Color(0xff0CBD3D),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30.0))
+                          : Text("Declinada",
+                              style: TextStyle(
+                                  color: Color(0xffBD0C0C),
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 30.0)),
+                    ],
+                  ),
+                  Text("Banco Popular Dominicano")
                 ],
               ),
             ],
