bbb41ee10f32f5ca304772f9a47cd1dd70bb0b13
David <davidbujosa@gmail.com>
2020-10-28 18:56:49 -0400

    Fix marging and padding in card preview widget

--- parka/lib/pages/user-payments/components/card_preview_widget.dart
+++ parka/lib/pages/user-payments/components/card_preview_widget.dart
@@ -1,7 +1,7 @@
 import 'package:ParkA/data/data-models/payment/payment_data_model.dart';
-import 'package:ParkA/pages/create-payment/components/credit_card_complete_info_form.dart';
 import 'package:ParkA/styles/text.dart';
 import "package:flutter/material.dart";
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CardListTile extends StatelessWidget {
   final Payment payment;
@@ -10,51 +10,48 @@ class CardListTile extends StatelessWidget {
     this.payment,
   });
 
-  Map<String, dynamic> gradientGetter(String creditCardNumber) {
-    if (creditCardNumber[0] == "4")
-      return CreditCardTypes.visa.gradient;
-    else if (creditCardNumber[0] == "5") {
-      return CreditCardTypes.mastercard.gradient;
-    }
-
-    return CreditCardTypes.unknown.gradient;
-  }
-
   @override
   Widget build(BuildContext context) {
     String visible = this.payment.digit.substring(12, 16);
 
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+      margin: EdgeInsets.symmetric(horizontal: 18.0, vertical: 2.0),
       padding: EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Text(
-              this.payment.cardHolder,
-              style: kParkaTextBaseStyleWhite.copyWith(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 36.0,
-              ),
+          Text(
+            this.payment.cardHolder,
+            style: kParkaTextBaseStyleWhite.copyWith(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 36.0,
             ),
           ),
           Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8.0),
-                child: Text(
-                  "•••• $visible",
-                  style: kParkaTextBaseStyleWhite.copyWith(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
+              Text("text"),
+              Column(
+                children: [
+                  Text(
+                    "•••• $visible",
+                    style: kParkaTextBaseStyleWhite.copyWith(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
-                ),
+                  Text("test"),
+                ],
               ),
-              Text("test"),
             ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15.0, right: 15.0, top: 15.0),
+            child: Divider(
+              color: Colors.black,
+              thickness: 0.9,
+              endIndent: 0,
+            ),
           ),
         ],
       ),
