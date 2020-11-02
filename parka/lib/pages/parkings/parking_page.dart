b95b8f24a4b199472372d4d8958fb5cd83427b5e
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-11-02 18:20:58 -0400

    created dummy parking page

---
+++ parka/lib/pages/parkings/parking_page.dart
@@ -0,0 +1,24 @@
+import 'package:ParkA/components/floating-action-button/parka_floating_action_button.dart';
+import "package:flutter/material.dart";
+import 'package:get/get.dart';
+
+class ParkingPage extends StatefulWidget {
+  static String routeName = 'parking-page';
+
+  @override
+  _ParkingPageState createState() => _ParkingPageState();
+}
+
+class _ParkingPageState extends State<ParkingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: ParkaFloatingActionButton(
+        iconData: Icons.add,
+        onPressedHandler: () {
+          // Get.toNamed(page);
+        },
+      ),
+    );
+  }
+}
