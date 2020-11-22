25527262dc5b4c71cc97c1a8b89bc0aafb5ac70a
Sebastiano Faiella <xeno097.cp@gmail.com>
2020-11-21 23:10:10 -0400

    added package shared preferencies

--- parka/lib/controllers/user_controller.dart
+++ parka/lib/controllers/user_controller.dart
@@ -1,14 +1,30 @@
 import 'package:ParkA/controllers/graphql_controller.dart';
 import 'package:ParkA/data/data-models/user/user_data_model.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ParkA/data/use-cases/user/user_use_cases.dart';
 import 'package:get/state_manager.dart';
 
 class UserController extends GetxController {
+  SharedPreferences _sharedPreferencies;
   Rx<User> user = null.obs;
+
+  void _initSharedPreferencies() async {
+    print("INIT SHARED PREFS");
+    this._sharedPreferencies = await SharedPreferences.getInstance();
+    String jwt = this._sharedPreferencies.getString("jwt");
+
+    if (jwt == null) {
+      print("NOT FOUND");
+    }
+  }
+
+  UserController() {
+    this._initSharedPreferencies();
+  }
 
   Future<bool> loginUser(String email, String password) async {
     User loggedUser = await UserUseCases.userLogin(email, password);
+
     if (loggedUser != null) {
       user.update((user) async {
         this.user = loggedUser.obs;
