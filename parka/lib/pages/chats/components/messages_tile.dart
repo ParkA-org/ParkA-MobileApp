b03a6a225fcfa25c38fdbf7da7db7309585ee623
bujosa <davidbujosa@gmail.com>
2020-11-25 21:08:01 -0400

    Fix right messages

--- parka/lib/pages/chats/components/messages_tile.dart
+++ parka/lib/pages/chats/components/messages_tile.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 
 class MessagesTile extends StatelessWidget {
@@ -20,9 +21,51 @@ class MessagesTile extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
       child: this.type == true
           ? Container(
-              child: Text("Prueba"),
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                  bottom: 8.0,
+                  left: 8.0,
+                  right: 10.0,
+                ),
+                child: AutoSizeText(
+                  this.messages,
+                  softWrap: true,
+                  style: TextStyle(
+                    fontFamily: "Montserrat",
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xff077187),
+              ),
             )
-          : Container( alignment: Alignment.bottomLeft ,child: Text(this.messages, style: TextStyle(fontFamily: "Montserrat", ),),
+          : Container(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 8.0,
+                  bottom: 8.0,
+                  left: 8.0,
+                  right: 10.0,
+                ),
+                child: AutoSizeText(
+                  this.messages,
+                  softWrap: true,
+                  style: TextStyle(
+                    fontFamily: "Montserrat",
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              decoration: BoxDecoration(
+                color: Color(0xff077187),
+              ),
+            ),
     );
   }
 }
