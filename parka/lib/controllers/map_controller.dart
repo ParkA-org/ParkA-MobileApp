55f1af013e7b1ebbc4eeed1bd3620f204fe6b74c
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-11-29 20:58:12 -0400

    PMB-57 Adds Set functions to the Map Controller

--- parka/lib/controllers/map_controller.dart
+++ parka/lib/controllers/map_controller.dart
@@ -1,6 +1,21 @@
+import 'package:ParkA/data/data-models/parking/parking_data_model.dart';
 import 'package:get/state_manager.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapController extends GetxController {
   Rx<GoogleMapController> mapController;
+  List<Parking> currentParking = List<Parking>().obs;
+
+  void setCurrentParkings(List<Parking> newParkings) {
+    newParkings.forEach((parking) {
+      if (!currentParking.contains(parking)) {
+        currentParking = newParkings.obs;
+        return;
+      }
+    });
+  }
+
+  void setMapController(GoogleMapController controller) {
+    mapController = controller.obs;
+  }
 }
