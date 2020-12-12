0ed53ab0e4eccdc463ff92d8f315565750d9310c
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-12-12 01:08:37 -0400

    PMB-50 Adds Header

--- parka/lib/pages/history/history_page.dart
+++ parka/lib/pages/history/history_page.dart
@@ -1,7 +1,13 @@
+import 'package:ParkA/data/data-models/reservation/reservation_data_model.dart';
+import 'package:ParkA/pages/create-vehicle/components/parka_resizable_on_scroll_app_bar.dart';
+import 'package:ParkA/pages/history/Components/reservation_placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class HistoryPage extends StatefulWidget {
-  HistoryPage({Key key}) : super(key: key);
+  final type;
+
+  HistoryPage({Key key, this.type}) : super(key: key);
 
   @override
   _HistoryPageState createState() => _HistoryPageState();
@@ -10,14 +16,47 @@ class HistoryPage extends StatefulWidget {
 }
 
 class _HistoryPageState extends State<HistoryPage> {
+  bool _loading;
+  List<Reservation> currentReservations = [];
+  Future<void> getAllUserReservations() async {
+    setState(() {
+      this._loading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loading = true;
+    getAllUserReservations();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("Hola"),
+        body: SafeArea(
+      child: ModalProgressHUD(
+        inAsyncCall: _loading,
+        child: RefreshIndicator(
+          onRefresh: this.getAllUserReservations,
+          child: CustomScrollView(
+            slivers: [
+              ParkaResizableOnScrollAppBar(
+                title: "Historial de Reservaciones",
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  !this._loading && this.currentReservations.length != 0
+                      ? Column(
+                          children: [],
+                        )
+                      : ReservationPlaceholder()
+                ]),
+              )
+            ],
+          ),
         ),
       ),
-    );
+    ));
   }
 }
