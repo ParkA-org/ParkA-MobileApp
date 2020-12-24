151e107f29047214bc6ba3590748b65643c33438
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-12-23 20:49:19 -0400

    PMB-30 Adds sign out and people API

--- parka/lib/controllers/login/google_sign_in.dart
+++ parka/lib/controllers/login/google_sign_in.dart
@@ -1,11 +1,26 @@
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/people/v1.dart';
+import 'package:http/http.dart' as http;
 
 class GoogleSignInController extends GetxController {
   void signIn() async {
     GoogleSignIn googleSignIn = GoogleSignIn(
-        scopes: ["https://www.googleapis.com/auth/contacts.readonly"]);
+        scopes: ["https://www.googleapis.com/auth/user.phonenumbers.read"]);
     var response = await googleSignIn.signIn();
-    print(response);
+
+    final authHeaders = await googleSignIn.currentUser.authHeaders;
+    await getContactInfo(authHeaders, response.id);
+    googleSignIn.signOut();
+  }
+
+  Future<String> getContactInfo(authHeader, accountId) async {
+    String url =
+        "https://people.googleapis.com/v1/people/$accountId?personFields=names,emailAddresses,phoneNumbers,birthdays";
+
+    var response = await http.get(url, headers: authHeader);
+
+    print(response.body);
+    return "Yes";
   }
 }
