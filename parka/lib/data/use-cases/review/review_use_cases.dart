b6cca95085c047749d8bdc5f802b444a2f39fef3
bujosa <davidbujosa@gmail.com>
2020-12-27 02:01:50 -0400

    Added review use cases class

--- parka/lib/data/use-cases/review/review_use_cases.dart
+++ parka/lib/data/use-cases/review/review_use_cases.dart
@@ -1,1 +1,71 @@
+import 'package:ParkA/controllers/graphql_controller.dart';
+import 'package:ParkA/data/dtos/reservation/create_reservation_dto.dart';
+import 'package:ParkA/pages/create-reservation/utils/generate_schedule_util.dart';
+import 'package:ParkA/utils/graphql/mutations/reservation_mutation.dart';
+import 'package:ParkA/data/data-models/reservation/reservation_data_model.dart';
+import 'package:ParkA/utils/graphql/queries/reservation_queries.dart';
+import 'package:get/get.dart';
+import 'package:graphql/client.dart';
 
+class ReviewUseCases {
+  static Future<bool> createReservation(
+      CreateReservationDto _createReservationDto) async {
+    final graphqlClient = Get.find<GraphqlClientController>()
+        .parkaGraphqlClient
+        .value
+        .graphQlClient;
+
+    final Map<String, dynamic> createReservationInput = {
+      "data": {
+        "parking": _createReservationDto.parking.id,
+        "owner": _createReservationDto.parking.user.id,
+        "checkInDate": _createReservationDto.checkInDate,
+        "checkOutDate": _createReservationDto.checkOutDate,
+        "vehicle": _createReservationDto.vehicle.id,
+        "paymentInfo": _createReservationDto.paymentInfo.id,
+        "total": _createReservationDto.total,
+        "rentDate": formatDate(_createReservationDto.rentDate),
+      }
+    };
+
+    final options = MutationOptions(
+        variables: createReservationInput,
+        documentNode: gql(createReservationMutation));
+
+    final _result = await graphqlClient.mutate(options);
+
+    if (_result.data != null) {
+      return true;
+    }
+
+    return false;
+  }
+
+  static Future<Reservation> getReservationById(id) async {
+    final graphqlClient = Get.find<GraphqlClientController>()
+        .parkaGraphqlClient
+        .value
+        .graphQlClient;
+
+    final data = {
+      "data": {
+        "id": id,
+      }
+    };
+
+    QueryOptions _queryOptions = new QueryOptions(
+        documentNode: gql(getReservationByIdQuery), variables: data);
+
+    final _result = await graphqlClient.query(_queryOptions);
+
+    if (_result.data != null) {
+      final reservation = _result.data['getReservationById'];
+
+      final Reservation reservationData =
+          Reservation.reservationFromJson(reservation);
+
+      return reservationData;
+    }
+    return null;
+  }
+}
